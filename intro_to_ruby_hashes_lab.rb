b970481f741aadfada937afbb352be98c7942187
Michael Jiang <jiangm1@yahoo.com>
2020-02-18 04:33:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,11 +30,5 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash = [key]
-    if hash[key]
-      hash[key] += 1 
-    else
-      hash[key] = 1
-    end
-    hash
+  
 end
