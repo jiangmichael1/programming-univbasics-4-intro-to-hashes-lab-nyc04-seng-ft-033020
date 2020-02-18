24820437487c2c961531a9b3f3e84481a3d895a5
Michael Jiang <jiangm1@yahoo.com>
2020-02-18 04:22:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,8 +31,8 @@ end
 
 def update_counting_hash(hash, key)
   hash = [key]
-    if hash = [key]
-      hash =
+    if hash[key]
+      hash[key] + 1
   
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
