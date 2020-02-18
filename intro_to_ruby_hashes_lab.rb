7458924afec2a90393517b5c4727398a151249c4
Michael Jiang <jiangm1@yahoo.com>
2020-02-18 04:34:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,9 +31,10 @@ end
 
 def update_counting_hash(hash, key)
   hash[key]
-  if hash[key] 
-    hash[key] += 1 
-  else 
-    hash[key] = 1
+    if hash[key] 
+      hash[key] += 1 
+    else 
+      hash[key] = 1
+    end
   hash
 end
